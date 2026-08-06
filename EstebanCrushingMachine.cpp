f768637252a5970010534ad86668d3c017d5f594
AngelicDemon <18223811+AngelicDemon@users.noreply.github.com>
2026-08-06 06:02:45 -0500

    Add extended key flag for media and volume keys.

--- EstebanCrushingMachine.cpp
+++ EstebanCrushingMachine.cpp
@@ -73,6 +73,12 @@ class Keyboard{
                 case VK_END:
                 case VK_PRIOR:
                 case VK_NEXT:
+                case VK_MEDIA_PLAY_PAUSE:
+                case VK_MEDIA_NEXT_TRACK:
+                case VK_MEDIA_PREV_TRACK:
+                case VK_VOLUME_MUTE:
+                case VK_VOLUME_DOWN:
+                case VK_VOLUME_UP:
                     input.ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
                     input.ki.wScan = static_cast<WORD>(MapVirtualKey(key, MAPVK_VK_TO_VSC_EX));
             }
